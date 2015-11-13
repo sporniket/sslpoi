9d3aeb7ac321b600e87329423ee19c282acaecd3
David SPORN <sporniket.studio@gmail.com>
2015-11-13 06:10:08 +0100

    DONE : first unit test for call ; TODO : fix grammar for call

---
+++ ssl-core/src/test/java/com/sporniket/scripting/ssl/CallTests.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package com.sporniket.scripting.ssl;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+
+import java_cup.runtime.ComplexSymbolFactory;
+import java_cup.runtime.ComplexSymbolFactory.ComplexSymbol;
+import java_cup.runtime.Symbol;
+
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNull;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sporniket.scripting.ssl.core.InitialisationMode;
+import com.sporniket.scripting.ssl.mass.Statement;
+import com.sporniket.scripting.ssl.mass.StatementDefineAs;
+import com.sporniket.scripting.ssl.mass.TreeBuilder;
+import com.sporniket.scripting.ssl.vess.AnalyzerLexical;
+import com.sporniket.scripting.ssl.vess.AnalyzerSyntaxic;
+import com.sporniket.scripting.ssl.vess.VessNode;
+import com.sporniket.scripting.ssl.vess.VessNodeCall;
+import com.sporniket.scripting.ssl.vess.VessNodeDefineAs;
+
+/**
+ * Test for "call ..." statements.
+ * 
+ * @author dsporn
+ *
+ */
+public class CallTests
+{
+	private AnalyzerSyntaxic myParser;
+
+	@Before
+	public void setupParserObjects()
+	{
+		final ComplexSymbolFactory _symbolFactory = new ComplexSymbolFactory();
+		final AnalyzerLexical _lexer = new AnalyzerLexical(null);
+		_lexer.setSymbolFactory(_symbolFactory);
+		setParser(new AnalyzerSyntaxic(_lexer, _symbolFactory));
+	}
+
+	private AnalyzerSyntaxic getParser()
+	{
+		return myParser;
+	}
+
+	private void setParser(AnalyzerSyntaxic parser)
+	{
+		myParser = parser;
+	}
+
+	@Test
+	public void testCorrectDefineAsNew() throws Exception
+	{
+		VessNodeCall _define = (VessNodeCall) TestUtils.parseVessSource("call foo", getParser());
+		assertThat(_define.getCall().getValue(), is("foo"));
+		assertThat(_define.getCall().isLastNode(), is(true));
+		assertThat(_define.getMapping(), nullValue());
+	}
+
+}
