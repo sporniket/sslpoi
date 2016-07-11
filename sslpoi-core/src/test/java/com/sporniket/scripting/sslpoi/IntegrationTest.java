c75317e8ba8f9a44029687d56e61705827c967d2
David SPORN <sporniket.studio@gmail.com>
2016-07-11 23:23:00 +0200

    implements the compilation sequence in the integration test

--- sslpoi-core/src/test/java/com/sporniket/scripting/sslpoi/IntegrationTest.java
+++ sslpoi-core/src/test/java/com/sporniket/scripting/sslpoi/IntegrationTest.java
@@ -5,9 +5,19 @@ package com.sporniket.scripting.sslpoi;
 
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import java_cup.runtime.ComplexSymbolFactory;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.sporniket.scripting.sslpoi.mass.Statement;
+import com.sporniket.scripting.sslpoi.mass.StatementFromNode;
+import com.sporniket.scripting.sslpoi.vess.AnalyzerLexical;
+import com.sporniket.scripting.sslpoi.vess.AnalyzerSyntaxic;
+import com.sporniket.scripting.sslpoi.vess.VessNode;
 
 /**
  * Test the convertion from a script to a list of {@link Statement}
@@ -16,6 +26,17 @@ import com.sporniket.scripting.sslpoi.mass.Statement;
  */
 public class IntegrationTest
 {
+	private AnalyzerSyntaxic myParser;
+
+	@Before
+	public void setupParserObjects()
+	{
+		final ComplexSymbolFactory _symbolFactory = new ComplexSymbolFactory();
+		final AnalyzerLexical _lexer = new AnalyzerLexical(null);
+		_lexer.setSymbolFactory(_symbolFactory);
+		setParser(new AnalyzerSyntaxic(_lexer, _symbolFactory));
+	}
+
 	private static final String[] SOURCE__V0_01 = {
 		"define callback1 as new com.foo.Callback",
 		"define callback2 as new com.foo.Callback",
@@ -32,8 +53,21 @@ public class IntegrationTest
 		"endon",
 	} ;
 	@Test
-	public void test__v0_01()
+	public void test__v0_01() throws Exception
 	{
+		String _source = TestUtils.makeSource(SOURCE__V0_01);
+		VessNode _parsed = TestUtils.parseVessSource(_source, getParser());
+		List<Statement> _compiled = new ArrayList<Statement>(StatementFromNode.convertNodeList(_parsed));
+		
+		
 		fail("not implemented yet");
+	}
+	public AnalyzerSyntaxic getParser()
+	{
+		return myParser;
+	}
+	public void setParser(AnalyzerSyntaxic parser)
+	{
+		myParser = parser;
 	}
 }
